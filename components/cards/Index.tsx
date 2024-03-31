100a1f03ec0b2eeb9e7bc96b90bf29a3741e3f13
Ronald <ronevanz01@gmail.com>
2024-03-31 07:17:28 -0500

    cards fix

--- components/cards/Index.tsx
+++ components/cards/Index.tsx
@@ -1,5 +1,8 @@
+"use client";
 import React from "react";
+import { motion, Variants } from "framer-motion";
 import { FiCreditCard, FiMail, FiUser, FiUsers } from "react-icons/fi";
+import { InView } from "react-intersection-observer";
 
 const HoverDevCards = () => {
   return (
@@ -9,41 +12,51 @@ const HoverDevCards = () => {
         <Card
           title="Educación a tu alcance"
           subtitle="Manage profile"
-          href="#"
-          Icon={FiUser}/>
+          Icon={FiUser}
+          index={0}/>
         <Card
           title="Docentes especializados"
           subtitle="Docentes capcitados para una educacion de calidad"
-          href="#" Icon={FiMail}/>
+          Icon={FiMail}
+          index={1}/>
         <Card
           title="A tu propio ritmo"
           subtitle="Puedes adaptar tu horario de estudios al resto de compromisos de tu dia"
-          href="#"
-          Icon={FiUsers}/>
+          Icon={FiUsers}
+          index={2}/>
         <Card
           title="Precios comodos"
           subtitle="Manage cards"
-          href="#"
-          Icon={FiCreditCard}/>
+          Icon={FiCreditCard}
+          index={3}/>
       </div>
     </div>
   );
 };
 
-const Card = ({ title, subtitle, Icon, href }: { title: string, subtitle: string, Icon: React.ElementType, href: string }) => {
+const Card = ({ title, subtitle, Icon, index }: { title: string, subtitle: string, Icon: React.ElementType, index:number }) => {
   return (
-    <a href={href}
-      className="w-full p-4 rounded border-[1px] border-slate-300 relative overflow-hidden group bg-white">
-      <div className="absolute inset-0 bg-gradient-to-r from-violet-600 to-indigo-600 translate-y-[100%] group-hover:translate-y-[0%] transition-transform duration-300" />
-      <Icon className="absolute z-10 -top-12 -right-12 text-9xl text-slate-100 group-hover:text-violet-400 group-hover:rotate-12 transition-transform duration-300" />
-      <Icon className="mb-2 text-5xl text-violet-600 group-hover:text-white transition-colors relative z-10 duration-300" />
-      <h3 className="font-medium text-lg text-slate-950 group-hover:text-white relative z-10 duration-300">
-        {title}
-      </h3>
-      <p className="text-slate-400 group-hover:text-violet-200 relative z-10 duration-300">
-        {subtitle}
-      </p>
-    </a>
+    <InView triggerOnce>
+      {({ inView, ref }) => (
+        <motion.div
+          ref={ref}
+          initial={{ opacity: 0, translateX: index % 2 === 0 ? -50 : 50, translateY: -50 }}
+          animate={inView ? { opacity: 1, translateX: 0, translateY: 0 } : {}}
+          transition={{ duration: 0.8, delay: index * 0.2 }}
+          className="w-full p-4 rounded border-[1px] border-slate-300 relative overflow-hidden group bg-white"
+        >
+          <div className="absolute inset-0 bg-gradient-to-r from-violet-600 to-indigo-600 translate-y-[100%] group-hover:translate-y-[0%] transition-transform duration-300" />
+          <Icon className="absolute z-10 -top-12 -right-12 text-9xl text-slate-100 group-hover:text-violet-400 group-hover:rotate-12 transition-transform duration-300" />
+          <Icon className="mb-2 text-5xl text-violet-600 group-hover:text-white transition-colors relative z-10 duration-300" />
+          <h3 className="font-medium text-lg text-slate-950 group-hover:text-white relative z-10 duration-300">
+            {title}
+          </h3>
+          <p className="text-slate-400 group-hover:text-violet-200 relative z-10 duration-300">
+            {subtitle}
+          </p>
+        </motion.div>
+      )}
+    </InView>
   );
 };
 
